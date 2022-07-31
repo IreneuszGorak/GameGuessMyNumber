4c7594c44ee401301eac501051eb18388e5a698f
IreneuszGorak <109158627+IreneuszGorak@users.noreply.github.com>
2022-07-31 11:33:17 +0200

    start project

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const rename = require('gulp-rename');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+
+gulp.task('sass', () => {
+  return gulp
+    .src('./sass/main.scss')
+    .pipe(rename('style.css'))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+});
