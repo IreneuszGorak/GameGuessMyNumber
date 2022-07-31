52201abbdb1ad2b278468e0fcbfdca65b3d9c64c
IreneuszGorak <109158627+IreneuszGorak@users.noreply.github.com>
2022-07-31 18:25:06 +0200

    css ready,  js start

---
+++ script/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+document.querySelector('.score').textContent = '5';
+document.querySelector('.highscore').textContent = '35';
+document.querySelector('.info-panel').textContent = 'Sialala';
